195163edb4e53d63a1cd7dac2a2bb9ff08b4ab8f
pandraungrs <pandraungs204@gmail.com>
2023-03-19 20:37:24 +0530

    crud operation and JPQL and Native Query also custom finder method

--- bootjpaexample1/src/main/java/com/jpa/test/Bootjpaexample1Application.java
+++ bootjpaexample1/src/main/java/com/jpa/test/Bootjpaexample1Application.java
@@ -15,19 +15,17 @@ public class Bootjpaexample1Application {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Bootjpaexample1Application.class, args);
-		
+
 		UserRepo userRepo = context.getBean(UserRepo.class);
 
-		
-		
-		//Insert Data
+		// Insert Data
 //		User user=new User();
 //		user.setUserName("Jay");
 //		user.setUserCity("Warje");
 //		user.setUserStatus("progress");
 //		userRepo.save(user);
-		
-		//Saving multiple user
+
+		// Saving multiple user
 //		User user1=new User();
 //		user1.setUserName("Maya");
 //		user1.setUserCity("Parje");
@@ -39,19 +37,17 @@ public class Bootjpaexample1Application {
 //		user2.setUserStatus("CREDIT");
 //		List<User> list = List.of(user1,user2);
 //		userRepo.saveAll(list);
-		
-			
-		//Get Data All
+
+		// Get Data All
 //		List<User> users=new ArrayList<>();
 //		users = (List<User>) userRepo.findAll();		
 //		users.forEach(usr -> System.out.println(usr));
-		
-		//Get Data using id
+
+		// Get Data using id
 //		User user = userRepo.findByUserId((long) 1);
 //		System.out.println(user);
-		
-		
-		//Update Data
+
+		// Update Data
 //		User user=userRepo.findByUserId((long) 1);
 //		System.err.println(user);
 //		
@@ -59,13 +55,42 @@ public class Bootjpaexample1Application {
 //		user.setUserCity("Nashik");
 //		user.setUserStatus("Pending");
 //		userRepo.save(user);
+
+		// Delete Data
+//		User user = userRepo.findByUserId((long) 1);
+//		userRepo.delete(user);
+//		System.out.println("Deleted");
+
+//		https://docs.spring.io/spring-data/jpa/docs/1.8.x/reference/html/#jpa.query-methods.query-creation
+
+//		List<User> result = userRepo.findByUserNameAndUserCity("Jay", "Warje");
+//
+//		result.forEach(usr -> {
+//			System.err.println(usr);
+//		});
+
+		List<User> allUser = userRepo.getAllUser();
+
+		allUser.forEach(usr -> {
+			System.out.println(usr);
+		});
+
+		List<User> userByName = userRepo.getUserByName("Jay");
+
+		userByName.forEach(u -> {
+			System.err.println(u);
+		});
+
+		List<User> userByNameAndCity = userRepo.getUserByNameAndCity("Maya", "Parje");
+		userByNameAndCity.forEach(uu -> {
+			System.err.println(uu);
+		});
 		
-		//Delete Data
-		User user=userRepo.findByUserId((long) 1);
-		userRepo.delete(user);
-		System.out.println("Deleted");
-		
-		
+		List<User> nativeQ = userRepo.getUsers();
+		nativeQ.forEach(uu -> {
+			System.err.println(uu);
+		});
+
 	}
 
 }
--- bootjpaexample1/src/main/java/com/jpa/test/Repository/UserRepo.java
+++ bootjpaexample1/src/main/java/com/jpa/test/Repository/UserRepo.java
@@ -1,6 +1,10 @@
 package com.jpa.test.Repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.jpa.test.entity.User;
 
@@ -8,7 +12,21 @@ public interface UserRepo extends CrudRepository<User, Long> {
 
 	User findByUserId(Long i);
 
+	List<User> findByUserNameAndUserCity(String userName, String userCity);
 	
+	@Query("select u FROM User u")
+	public List<User> getAllUser();
+
+	
+	@Query("select u FROM User u where u.userName =:n")
+	public List<User> getUserByName(@Param("n") String name);
+	
+	
+	@Query("select u FROM User u where u.userName =:n and u.userCity =:c")
+	public List<User> getUserByNameAndCity(@Param("n") String name,@Param("c") String city);
+	
+	@Query(value = "select * from user_table", nativeQuery = true)
+	public List<User> getUsers();
 
 
 
